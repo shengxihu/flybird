41412410dfe0368421aeeb5bfb7025436e7bd130
shengxihu <1679112936@qq.com>
2016-10-18 11:53:46 +0800

    测试

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var high = 0;
+var lef = 0;
 var time;
 function test (){
 	var t = 0;
 	var bird = document.getElementById('bird');
+	var fbi = document.getElementById('fbi');
 	time = setInterval(function(){
-		var btm = parseFloat(bird.offsetTop);
+		var btm_1 = parseFloat(bird.offsetTop);
+		var btm_2 = parseFloat(fbi.offsetTop);
+		var lef_1 = parseFloat(bird.offsetLeft);
+		var lef_2 = parseFloat(fbi.offsetLeft);
 	 	t += 1;
-	 	if (high < 700 && btm <380) {
+	 	if (high < 700 && btm_1 <380) {
 	 		high += (Math.pow(t,2))/2;
+	 		lef += 10;
+	 		if (lef_1 > lef_2 - 20 && btm_1<100) {
+	 			alert("碰到障碍物，游戏结束！")
+		 		clearTimeout(time);
+		 		return false;
+	 		}
 	 	} else {
 	 		alert("碰到下边界，游戏结束！")
 	 		clearTimeout(time);
@@ -15,6 +26,7 @@ function test (){
 	 	}
 	 	console.log(high);
 	 	bird.style.marginTop = high + 'px';
+	 	fbi.style.right = lef + 'px';
 	},100)
 }
 
